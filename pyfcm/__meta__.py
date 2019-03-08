e715a177b8f24b9ae126cd77025453a9cc3b2b94
Christy O'Reilly <christy@oreills.co.uk>
2019-03-08 09:26:35 +0000

    Add configurable retries to info endpoint

--- pyfcm/__meta__.py
+++ pyfcm/__meta__.py
@@ -5,7 +5,7 @@ __title__ = 'pyfcm'
 __summary__ = 'Python client for FCM - Firebase Cloud Messaging (Android, iOS and Web)'
 __url__ = 'https://github.com/olucurious/pyfcm'
 
-__version__ = '1.4.5'
+__version__ = '1.4.6'
 
 __install_requires__ = ['requests', 'requests-toolbelt']
 
