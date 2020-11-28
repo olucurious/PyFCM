3f20e7f4e0b4bdc48e4bf274ea87376650b9b286
olucurious <olucurious@gmail.com>
2020-11-28 18:19:43 +0100

    update version

--- pyfcm/__meta__.py
+++ pyfcm/__meta__.py
@@ -2,7 +2,7 @@ __title__ = 'pyfcm'
 __summary__ = 'Python client for FCM - Firebase Cloud Messaging (Android, iOS and Web)'
 __url__ = 'https://github.com/olucurious/pyfcm'
 
-__version__ = '1.4.6'
+__version__ = '1.4.8'
 
 __author__ = 'Emmanuel Adegbite'
 __email__ = 'olucurious@gmail.com'
