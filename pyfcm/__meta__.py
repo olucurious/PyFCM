d99111411cde4071eecf81bf4cd430635dee5b78
Łukasz Rogowski <rogal1703@gmail.com>
2021-06-10 14:54:13 +0200

    - Replace deprecated urllib3.Retry option v2

--- pyfcm/__meta__.py
+++ pyfcm/__meta__.py
@@ -2,7 +2,7 @@ __title__ = 'pyfcm'
 __summary__ = 'Python client for FCM - Firebase Cloud Messaging (Android, iOS and Web)'
 __url__ = 'https://github.com/olucurious/pyfcm'
 
-__version__ = '1.5.1'
+__version__ = '1.4.9'
 
 __author__ = 'Emmanuel Adegbite'
 __email__ = 'olucurious@gmail.com'
