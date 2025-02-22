a04f8623e06e24d70651ca5131e9f489aefdd845
Emmanuel O. Adegbite <olucurious@gmail.com>
2025-02-22 20:38:27 +0100

    Update __meta__.py

--- pyfcm/__meta__.py
+++ pyfcm/__meta__.py
@@ -2,7 +2,7 @@ __title__ = "pyfcm"
 __summary__ = "Python client for FCM - Firebase Cloud Messaging (Android, iOS and Web)"
 __url__ = "https://github.com/olucurious/pyfcm"
 
-__version__ = "2.0.7"
+__version__ = "2.0.8"
 
 __author__ = "Emmanuel Adegbite"
 __email__ = "olucurious@gmail.com"
